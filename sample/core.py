09eb9420d95d9d26e4d81418f9e9a27a1cf874dd
tsuphona <thomas.suphona@gmail.com>
2023-01-20 15:51:40 +0100

    Add docstring to add function

--- sample/core.py
+++ sample/core.py
@@ -13,9 +13,40 @@ def hmm() -> None:
         print(get_hmm())
 
 
-def add(a: int, b: int) -> int:
-    """Add two integers"""
-    return a + b
+def add(num1: int, num2: int) -> int:
+    """
+    Add up two integer numbers.
+
+    This function simply wraps the ``+`` operator, and does not
+    do anything interesting, except for illustrating what
+    the docstring of a very simple function looks like.
+
+    Parameters
+    ----------
+    num1 : int
+        First number to add.
+    num2 : int
+        Second number to add.
+
+    Returns
+    -------
+    int
+        The sum of ``num1`` and ``num2``.
+
+    See Also
+    --------
+    subtract : Subtract one integer from another.
+
+    Examples
+    --------
+    >>> add(2, 2)
+    4
+    >>> add(25, 0)
+    25
+    >>> add(10, -10)
+    0
+    """
+    return num2 + num2
 
 
 def subtract(a: int, b: int) -> int:
